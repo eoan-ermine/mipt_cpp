f0f9ee4f3426f033e89b6d1ba3244af8f11c19fe
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-05 23:22:13 +0500

    Add demonstration code

--- Graduate C++/Scent of C++/LRU/main.cpp
+++ Graduate C++/Scent of C++/LRU/main.cpp
@@ -1,3 +1,22 @@
+#include "lru.hpp"
+
+#include <iostream>
+
+struct DummyType {
+    int id;
+};
+
 int main() {
-    
+    int cache_capacity, count_of_elements;
+    std::cin >> cache_capacity >> count_of_elements;
+    LRUCache<DummyType> cache(cache_capacity);
+
+    int cache_hit = 0;
+    while(count_of_elements--) {
+        int key; std::cin >> key;
+        if(cache.put(key, DummyType{ key })) {
+            cache_hit += 1;
+        }
+    }
+    std::cout << cache_hit << std::endl;
 }
