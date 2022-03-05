d21c835e2cc6a1854aa5ba9a3bd0fe1ff8347b9d
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-05 23:21:19 +0500

    Move all to .hpp, change return type of put, minor changes
    * Move all to .hpp, cause it's templates
    * `put` method returns if it cache hit or not now
    * Minor fixes

--- Graduate C++/Scent of C++/LRU/lru.hpp
+++ Graduate C++/Scent of C++/LRU/lru.hpp
@@ -2,6 +2,8 @@
 #include <unordered_map>
 #include <list>
 
+#include <stdexcept>
+
 template<typename Value, typename Key = int>
 class LRUCache {    
     using ListIt = typename std::list<Value>::iterator;
@@ -10,7 +12,28 @@ class LRUCache {
     std::list<Value> values;
     std::size_t capacity_;
 public:
-    LRUCache(std::size_t capacity);
-    Value get(Key key);
-    void put(Key key, Value value);
+    LRUCache(std::size_t capacity)
+        : capacity_(capacity) {}
+
+    Value get(Key key) {
+        if(auto it = lookupTable.find(key); it != lookupTable.end()) {
+            values.splice(values.begin(), values, it->second);
+            return *(it->second);
+        }
+        throw std::runtime_error{"There are no such value in cache"};
+    }
+
+    bool put(Key key, Value value) {
+        if(auto it = lookupTable.find(key); it != lookupTable.end()) {
+            values.splice(values.begin(), values, it->second);
+            return true;
+        }
+        if(values.size() == capacity_) {
+            lookupTable.erase(values.back().id);
+            values.pop_back();
+        }
+        values.push_front(value);
+        lookupTable[key] = values.begin();
+        return false;
+    }
 };
