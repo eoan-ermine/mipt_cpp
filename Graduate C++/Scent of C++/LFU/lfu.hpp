d1f1464de6aafc678752689564263bd3576a54a3
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-05 23:42:14 +0500

    Implement LFU cache, add demonstration example

--- Graduate C++/Scent of C++/LFU/lfu.hpp
+++ Graduate C++/Scent of C++/LFU/lfu.hpp
@@ -1,12 +1,57 @@
+#include <unordered_map>
+#include <stdexcept>
 #include <cstdint>
+#include <utility>
+#include <list>
+
+constexpr int MAXIMUM_FREQUENCY = 65536;
 
 template<typename Value, typename Key = int>
 class LFUCache {
+    using ListIt = typename std::list<Value>::iterator;
+
+    std::unordered_map<Key, std::pair<int, ListIt>> lookupTable;
+    std::list<Value> values;
     std::size_t capacity_;
+
+    auto get_least_frequently_used() {
+        int minimum_frequency = MAXIMUM_FREQUENCY;
+        auto minimum_frequency_iterator = lookupTable.end();
+
+        for(auto it = lookupTable.begin(), end = lookupTable.end(); it != end; ++it) {
+            if(auto curFrequency = it->second.first; curFrequency <= minimum_frequency) {
+                minimum_frequency = curFrequency;
+                minimum_frequency_iterator = it;
+            }
+        }
+
+        return minimum_frequency_iterator;
+    }
 public:
     LFUCache(std::size_t capacity)
-        : capacity_(capacity) {}
+        : capacity_(capacity) { }
 
-    Value get(Key key);
-    bool put(Key key, Value value);
+    Value get(Key key) {
+        if(auto it = lookupTable.find(key); it != lookupTable.end()) {
+            it->second->first += 1;
+            values.splice(values.begin(), values, it->second.second);
+            return *(it->second->second);
+        }
+        throw std::runtime_error{"There are no such key in cache"};
+    }
+
+    bool put(Key key, Value value) {
+        if(auto it = lookupTable.find(key); it != lookupTable.end()) {
+            values.splice(values.begin(), values, it->second.second);
+            return true;
+        }
+        if(values.size() == capacity_) {
+            auto to_delete = get_least_frequently_used();
+            values.erase(to_delete->second.second);
+            lookupTable.erase(to_delete);
+        }
+        values.push_front(value);
+        lookupTable[key] = {1, values.begin()};
+        return false;
+    }
 };
--- Graduate C++/Scent of C++/LFU/main.cpp
+++ Graduate C++/Scent of C++/LFU/main.cpp
@@ -1,3 +1,20 @@
 #include "lfu.hpp"
 
-int main() { }
+#include <iostream>
+
+struct DummyType { };
+
+int main() {
+    int cache_capacity, count_of_elements;
+    std::cin >> cache_capacity >> count_of_elements;
+    LFUCache<DummyType> cache(cache_capacity);
+
+    int cache_hit = 0;
+    while(count_of_elements--) {
+        int key; std::cin >> key;
+        if(cache.put(key, DummyType{})) {
+            cache_hit += 1;
+        }
+    }
+    std::cout << cache_hit << std::endl;
+}
