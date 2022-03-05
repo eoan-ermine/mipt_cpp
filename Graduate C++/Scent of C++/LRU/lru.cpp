0183f165f42fcb2c91de042dafa4319717e17591
PatriotRossii <patriotrossii2019@mail.ru>
2022-03-05 23:09:02 +0500

    Implement LRU cache

--- Graduate C++/Scent of C++/LRU/lru.cpp
+++ Graduate C++/Scent of C++/LRU/lru.cpp
@@ -1,16 +1,30 @@
 #include "lru.hpp"
 
-template<typename Value, typename Key>
-LRUCache<Value, Key>::LRUCache(std::size_t capacity) {
+#include <stdexcept>
 
-}
+template<typename Value, typename Key>
+LRUCache<Value, Key>::LRUCache(std::size_t capacity)
+    : capacity_(capacity) {}
 
 template<typename Value, typename Key>
 Value LRUCache<Value, Key>::get(Key key) {
-
+    if(auto it = lookupTable.find(key); it != lookupTable.end()) {
+        values.splice(lookupTable.begin(), lookupTable, it);
+        return *it;
+    }
+    throw std::runtime_error{"There are no such value in cache"};
 }
 
 template<typename Value, typename Key>
 void LRUCache<Value, Key>::put(Key key, Value value) {
-
+    if(auto it = lookupTable.find(key); it != lookupTable.end()) {
+        values.splice(lookupTable.begin(), lookupTable, it->second);
+    } else {
+        if(values.size() == capacity_) {
+            lookupTable.erase(values.back().id);
+            values.pop_back();
+        }
+        values.push_front(value);
+        lookupTable[key] = values.begin();
+    }
 }
--- Graduate C++/Scent of C++/LRU/lru.hpp
+++ Graduate C++/Scent of C++/LRU/lru.hpp
@@ -1,7 +1,14 @@
 #include <cstdint>
+#include <unordered_map>
+#include <list>
 
 template<typename Value, typename Key = int>
-class LRUCache {
+class LRUCache {    
+    using ListIt = typename std::list<Value>::iterator;
+
+    std::unordered_map<Key, ListIt> lookupTable;
+    std::list<Value> values;
+    std::size_t capacity_;
 public:
     LRUCache(std::size_t capacity);
     Value get(Key key);
